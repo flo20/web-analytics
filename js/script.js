bc9f8232c6c57fe18335aaa05e4ddc29315d5578
flo20 <59807384+flo20@users.noreply.github.com>
2020-03-05 13:55:01 +0200

    Second commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log('That was it');
+
+var a= 3;
+var b= 2;
+var c= a + b;
+
+if (a > b) { 
+    // do the following if a is lesser than b
+    console.log('a is lesser than b');
+    console.log (a  + ' is lesser than ' +b);
+} 
+else{
+    //do the following things otherwise
+    console.log('a is not greater than b');
+}
+if (a === b) { 
+    console.log('a is exzactly 1');
+}
